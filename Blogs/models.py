9b29464fdd436ce56622387329174cf5877ca5d8
ShubhamRaj <sahilraj2310@gmail.com>
2023-08-04 21:34:52 +0530

    commit03

--- Blogs/models.py
+++ Blogs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.timezone import datetime
 
 # Create your models here.
+class ContactTb(models.Model):
+    name = models.CharField(max_length=50,)
+    email = models.EmailField(max_length=70)
+    phone = models.IntegerField()
+    address = models.CharField(max_length=50)
+    message = models.TextField()
+    # created_at = models.DateTimeField(auto_now_add=True, default=datetime.now())
+    
+    def __str__(self) -> str:
+        return f"{self.name} - {self.email}"
--- Blogs/views.py
+++ Blogs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import ContactTb
+from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
@@ -12,4 +13,21 @@ def all_blogs(request):
     return render(request,'all_blogs.html')
 
 def contact(request):
+
+    if request.method == 'POST':
+        name = request.POST['fullname']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        address = request.POST['add']
+        message = request.POST['msg']
+
+        if len(name) > 1 and len(email) >15 and len(str(phone)) == 10 and len(address) > 15 and len(message) > 20:
+            contactObj = ContactTb(name=name,
+                                email=email, phone=phone,
+                                address=address,message=message,)
+            contactObj.save()
+
+        else :
+            return HttpResponse('Please fill valid information.')
+
     return render(request,'contact.html')
