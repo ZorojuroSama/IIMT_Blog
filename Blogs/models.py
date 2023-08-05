32465198e268753f17c1fce2e3491e2be7b4c80c
ShubhamRaj <sahilraj2310@gmail.com>
2023-08-06 02:26:49 +0530

    changes 06-08-23

--- Blogs/models.py
+++ Blogs/models.py
@@ -3,12 +3,12 @@ from django.utils.timezone import datetime
 
 # Create your models here.
 class ContactTb(models.Model):
+    sno = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50,)
     email = models.EmailField(max_length=70)
     phone = models.IntegerField()
     address = models.CharField(max_length=50)
     message = models.TextField()
     # created_at = models.DateTimeField(auto_now_add=True, default=datetime.now())
-    
-    def __str__(self) -> str:
-        return f"{self.name} - {self.email}"
+    def __str__(self):
+        return f"{self.name}, {self.email}"
